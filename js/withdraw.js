40aad49754b9674a2620b2f00a2e4a80f92cbb6d
ShahabMat <cse_1812020090@lus.ac.bd>
2023-08-15 17:06:05 +0600

    complete the robbery bank ltd

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,7 +1,14 @@
  /* 
     1. add event handler with the withdraw button 
     2. get the withdraw amount from the withdraw input field
-    2.5: also make sure to convert the input into a number by using parseFloat
+    2-5: also make sure to convert the input into a number by using parseFloat
+    3. Get previous withdraw total 
+    4. Calculate total withdraw amount
+    4-5. Set total withdraw amount
+    6. Get the previous balance total amount
+    7. Calculate new balance total
+    7-5. Set the new balance total
+
 */
     //step-1:
 document.getElementById('btn-withdraw').addEventListener('click', function(){
@@ -26,7 +33,7 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
     
     // step-7:
-    const currentTotalBalance = previousBalanceTotal - currentWithdrawTotal;
+    const currentTotalBalance = previousBalanceTotal - newWithdraw;
     //
     balanceTotalElement.innerText = currentTotalBalance;
 
